ca5d2aad23566b567bb0bc0826805db632e797d2
ifeanyi_ns <ifeanyi_ns@icloud.com>
2024-03-10 08:27:13 +0100

    task

---
+++ React-TodoList/src/components/Header.tsx
@@ -0,0 +1,15 @@
+
+import Button from './Button';
+
+const Header = (props: { text: string }) => {
+  return (
+    <div className='header-cont'>
+      <h3 className='header-text'>{props.text}</h3>
+      <span className='header-btn'><Button btnText={'Add'} bgcolor={'green'} color={'white'}/></span>
+    </div>
+  )
+}
+
+
+export default Header
+
